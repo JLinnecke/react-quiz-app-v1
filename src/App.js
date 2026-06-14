54f892ab2225faa73789a53e3615ddd5b51711cd
🎭 🎪 <jlinnecke@gmail.com>
2026-06-14 21:12:11 +0200

    add answer state and button disabling logic

--- src/App.js
+++ src/App.js
@@ -40,35 +40,77 @@ function Header() {
 
 function Quiz() {
   const [currQuestion, setCurrQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
 
   const currentQuestion = questionList[currQuestion];
 
+  function handleAnswer(answer) {
+    setSelectedAnswer(answer);
+  }
+
   return (
     <section className="quiz-card">
-      <Question question={currentQuestion} />
+      <Question
+        question={currentQuestion}
+        selectedAnswer={selectedAnswer}
+        onAnswer={handleAnswer}
+      />
     </section>
   );
 }
 
-function Question({ question }) {
+function Question({ question, onAnswer, selectedAnswer }) {
   return (
     <div className="question-headline">
       <h2>{question.question}</h2>
-      <AnswerList question={question} />
+      <AnswerList
+        question={question}
+        onAnswer={onAnswer}
+        selectedAnswer={selectedAnswer}
+      />
     </div>
   );
 }
 
-function AnswerList({ question }) {
+function AnswerList({ question, onAnswer, selectedAnswer }) {
   return (
     <div className="answer-card">
       {question.answers.map((answer) => (
-        <Button key={answer}>{answer}</Button>
+        <Button
+          key={answer}
+          onClick={() => onAnswer(answer)}
+          disabled={selectedAnswer !== null}
+          className={
+            selectedAnswer === answer
+              ? answer === question.correctAnswer
+                ? "correct"
+                : "wrong"
+              : ""
+          }
+        >
+          {answer}
+        </Button>
       ))}
+
+      <div className="progress">
+        <p>1 / 3 Questions</p>
+      </div>
+      <div className="next-question">
+        <Button
+          className={selectedAnswer !== null ? "active" : "next-btn"}
+          disabled={selectedAnswer === null}
+        >
+          next
+        </Button>
+      </div>
     </div>
   );
 }
 
-function Button({ children }) {
-  return <button>{children}</button>;
+function Button({ children, onClick, className, disabled }) {
+  return (
+    <button onClick={onClick} disabled={disabled} className={className}>
+      {children}
+    </button>
+  );
 }
